0510b4ed897dbec5f5162e8d83134a9c8d875384
EC2 Default User <jun63336543@gmail.com>
2020-08-22 13:03:02 +0000

    kadai-column

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -27,7 +27,7 @@ class TasksController < ApplicationController
       @task = Task.find(params[:id])
   end
 
-   def update
+  def update
     @task = Task.find(params[:id])
 
     if @task.update(task_params)
@@ -40,7 +40,7 @@ class TasksController < ApplicationController
   end
 
   def destroy
-     @task = Task.find(params[:id])
+    @task = Task.find(params[:id])
     @task.destroy
 
     flash[:success] = 'Task は正常に削除されました'
@@ -49,9 +49,13 @@ class TasksController < ApplicationController
 
 
  private
- 
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
   # Strong Parameter
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :status)
   end
 end
